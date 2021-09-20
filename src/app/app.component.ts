a9f62ee0e1bdf1b71c64c88d63835b64873b81b4
williamisraelmt <williamisrael1601@outlook.es>
2021-09-20 12:53:10 -0400

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,28 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' },
+      ],
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [{ id: 7, name: 'subsub' }],
+        },
+      ],
+    },
+  ];
+  options = {};
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TreeModule } from '@circlon/angular-tree-component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, TreeModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
